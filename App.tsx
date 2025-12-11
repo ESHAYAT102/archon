33443b3e9bf9da7b4431097f4cbb63023866a5b0
Eshayat Al-Wasiu <eshayat.wasiu@gmail.com>
2025-12-11 15:21:17 +0600

    seperate curl command for dotfiles added

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import CodeBlock from "./components/CodeBlock";
 
 const App: React.FC = () => {
   const setupCommand = "curl -s https://archon.eshayat.com/script.sh | bash";
+  const dotfilesCommand= "curl -s https://archon.eshayat.com/dotfiles.sh | bash";
 
   return (
     <main className="bg-[#1e1e2e] text-[#cdd6f4] min-h-screen flex flex-col items-center justify-center font-sans p-4 selection:bg-[#a6e3a1] selection:text-[#1e1e2e]">
@@ -11,9 +12,16 @@ const App: React.FC = () => {
           <span className="text-[#a6e3a1]">Arch</span>on
         </h1>
         <p className="text-lg md:text-xl text-[#bac2de] mb-8">
-          The one-command setup for a clean Arch Linux environment.
+          The one-command setup for a clean Omarchy Arch Linux environment.
+        </p>
+        <p className="mb-4">
+          Full setup:
         </p>
         <CodeBlock command={setupCommand} />
+        <p className="mb-4 mt-8">
+          Just the dotfiles:
+        </p>
+        <CodeBlock command={dotfilesCommand} />
       </div>
     </main>
   );
