b38ea319e97032ca0077833463085636adb86dab
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2025-12-14 22:32:01 +0600

    Update script.sh

--- dist/script.sh
+++ dist/script.sh
@@ -17,11 +17,8 @@ flatpak install --or-update com.github.neithern.g4music app.zen_browser.zen com.
 
 echo "--- Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 echo "" >> "$HOME/.config/fish/config.fish"
-
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.config/fish/config.fish"
-
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 echo "--- Installing Ollama..."
@@ -34,10 +31,11 @@ echo "--- Configuring global Git settings..."
 git config --global init.defaultBranch main
 git config --global --replace-all credential.helper store
 
+echo "--- Installing Omarchy Themes..."
 omarchy-theme-install https://github.com/ESHAYAT102/omarchy-catppuccin-green-theme
-
 omarchy-theme-install https://github.com/ESHAYAT102/omarchy-catppuccin-mauve-theme
 
+echo "--- Setting up dotfiles..."
 curl -s https://archon.eshayat.com/dotfiles.sh | bash
 
 echo "--- Script finished! Please reboot."
