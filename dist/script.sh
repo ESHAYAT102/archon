544ab18ea276b995f62541591436084bafea3685
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2025-12-14 22:25:23 +0600

    Update script.sh

--- dist/script.sh
+++ dist/script.sh
@@ -7,7 +7,7 @@ clear
 
 # 1. REMOVE unwanted packages
 echo "--- Removing selected packages..."
-sudo pacman -R --noconfirm 1password-cli 1password-beta kdenline limine-snapper-sync signal-desktop xournalpp
+sudo pacman -R --noconfirm 1password-cli 1password-beta kdenline limine-snapper-sync signal-desktop xournalpp typora
 
 # 2. UPDATE system
 echo "--- Updating system packages..."
