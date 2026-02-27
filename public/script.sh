a0c76162c4a93eb41a2bf6b9372a1590768cb0df
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2026-02-28 04:35:29 +0600

    Update script.sh

--- public/script.sh
+++ public/script.sh
@@ -2,7 +2,7 @@
 
 clear
 sudo pacman -Syu --noconfirm
-sudo pacman -R --noconfirm 1password-cli 1password-beta kdenlive limine-snapper-sync signal-desktop xournalpp typora mako swayosd
+sudo pacman -R --noconfirm 1password-cli 1password-beta kdenlive limine-snapper-sync signal-desktop xournalpp typora mako swayosd libreoffice-fresh
 rm -rf ~/.local/share/omarchy/applications/typora.desktop
 sudo pacman -S --noconfirm flatpak fish tmux ghostty 7zip yazi btop tree tldr bat thefuck kew swaync nano cava go cpio cmake cmatrix stow lib32-mesa lib32-vulkan-intel vulkan-intel visual-studio-code-bin glow vhs skate shotwell zip unzip wget curl vlc hyprshot ttf-firacode-nerd ttf-cascadia-code ttf-iosevka-nerd
 yay -S --noconfirm vicinae-bin tty-clock spicetify-cli
