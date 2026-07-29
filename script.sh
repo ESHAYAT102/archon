2b720bbd4a892fd39536fb548379cbd4839f6cae
Eshayat Al-Wasiu <eshayat.wasiu@gmail.com>
2026-07-30 04:12:01 +0600

    Use Zsh as the default shell

--- script.sh
+++ script.sh
@@ -26,7 +26,7 @@ pacman_packages_remove=(
 
 pacman_packages_install=(
   flatpak
-  fish
+  zsh
   ghostty
   tmux
   7zip
@@ -213,7 +213,7 @@ xdg-settings set default-web-browser app.zen_browser.zen.desktop
 xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/http
 xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/https
 xdg-mime default app.zen_browser.zen.desktop text/html
-chsh -s /usr/bin/fish
+chsh -s "$(command -v zsh)"
 
 gsettings set org.gtk.gtk4.Settings.Debug enable-inspector-keybinding false
 gsettings set org.gtk.Settings.Debug enable-inspector-keybinding false
