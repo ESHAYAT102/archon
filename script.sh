6064693ac562c548d45b1f49d39fb96371bab318
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2026-08-03 15:04:43 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -236,6 +236,9 @@ curl -fsSL https://raw.githubusercontent.com/ESHAYAT102/vicinae-confetti-extensi
 curl -fsSL https://herdr.dev/install.sh | sh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> $HOME/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"' >> $HOME/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 for package in ${brew_package_install[@]}; do
   brew install ${package}
 done
